d00d929f3a0554eeea09e4db2cd8f3f100fb7eec
art <57792318+artshkreba@users.noreply.github.com>
2024-07-26 03:58:59 +0200

    update

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,144 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+- Use the top 10 highest-paying Data Analyst jobs from first query
+- Add the specific skills required for these roles
+- Why? It provides a detailed look at which high-paying jobs demand certain skills, 
+    helping job seekers understand which skills to develop that align with top salaries
+*/
+
+WITH top_paying_jobs AS (
+    SELECT  
+        job_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date,
+        name AS company_name
+    FROM    
+        job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere' AND
+        salary_year_avg IS NOT NULL
+    ORDER BY
+        salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    skills 
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC 
+
+
+/* 
+Here's an overview of the leading data analyst positions in 2023:
+
+Broad Salary Spectrum: The top 10 highest-paying data analyst positions offer salaries ranging from $184,000 to $650,000, highlighting the substantial earning potential within the profession.
+
+Range of Employers: High salaries are being offered by a diverse set of companies, including SmartAsset, Meta, and AT&T, demonstrating widespread demand across various industries.
+
+Variety of Job Titles: The field features a wide array of job titles, such as Data Analyst and Director of Analytics, indicating the different roles and specializations available in data analytics.
+
+RESULTS
+=======
+[
+  {
+    "job_id": 226942,
+    "job_title": "Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "650000.0",
+    "job_posted_date": "2023-02-20 15:13:33",
+    "company_name": "Mantys"
+  },
+  {
+    "job_id": 547382,
+    "job_title": "Director of Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42",
+    "company_name": "Meta"
+  },
+  {
+    "job_id": 552322,
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12",
+    "company_name": "AT&T"
+  },
+  {
+    "job_id": 99305,
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40",
+    "company_name": "Pinterest Job Advertisements"
+  },
+  {
+    "job_id": 1021647,
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23",
+    "company_name": "Uclahealthcareers"
+  },
+  {
+    "job_id": 168310,
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 731368,
+    "job_title": "Director, Data Analyst - HYBRID",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189309.0",
+    "job_posted_date": "2023-12-07 15:00:13",
+    "company_name": "Inclusively"
+  },
+  {
+    "job_id": 310660,
+    "job_title": "Principal Data Analyst, AV Performance Analysis",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189000.0",
+    "job_posted_date": "2023-01-05 00:00:25",
+    "company_name": "Motional"
+  },
+  {
+    "job_id": 1749593,
+    "job_title": "Principal Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "186000.0",
+    "job_posted_date": "2023-07-11 16:00:05",
+    "company_name": "SmartAsset"
+  },
+  {
+    "job_id": 387860,
+    "job_title": "ERM Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "184000.0",
+    "job_posted_date": "2023-06-09 08:01:04",
+    "company_name": "Get It Recruit - Information Technology"
+  }
+]
+*/
+
